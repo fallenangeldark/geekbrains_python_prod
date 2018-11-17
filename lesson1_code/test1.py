5396637e24e140823b5784d0f7f59543fd38dbe9
Pete <piedbox@gmail.com>
2018-11-17 17:34:56 +1000

    Lesson_1 complete

---
+++ lesson1_code/test1.py
@@ -0,0 +1,17 @@
+import subprocess
+
+args = ["ping", "www.yahoo.com"]
+process = subprocess.Popen(args, stdout=subprocess.PIPE, encoding='cp866')
+data = process.communicate()
+for el in data:
+    if el != None:
+        print(el, end='\n')
+    #Отсекаем stderr
+    continue
+
+
+code = subprocess.call("notepad.exe")
+if code == 0:
+    print("Success!")
+else:
+    print("Error!")
