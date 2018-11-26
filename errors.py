f019a36172f754d762fca336cf58a0026cd3c67d
Pete <piedbox@gmail.com>
2018-11-26 20:07:52 +1000

    exercise 3

---
+++ errors.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+""" Errors """
+
+#Исключение когда имя пользователя слишком длинное
+class UsernameToLongError(Exception):
+    def __init__(self, username):
+        self.username = username
+
+    def __str__(self):
+        return "Имя пользователя {} должно быть менее 26 символов".format(self.username)
+
+#Исключение, переданный код отсутствует среди стандартных кодов
+class ResponseCodeError(Exception):
+    def __init__(self, code):
+        self.code  = code
+
+    def __str__(self):
+        return "Неверный код ответа {}".format(self.code)
+
+#исключение, длина кода - не три символа
+class ResponseCodeLenError(ResponseCodeError):
+    def __str__(self):
+        return "Неверная длина кода {}. Длина кода должна быть 3 символа".format(self.code)
+
+#Исключение, отсутствует обязательный атрибут response
+class MandatoryKeyError(Exception):
+    def __init__(self, key):
+        self.key = key
+
+    def __str__(self):
+        return 'Не хватает обязательного атриута {}'.format(self.key)
---
+++ ex_3_server.py
@@ -0,0 +1,69 @@
+"""
+Функции сервера:
+ - принимает сообщение клиента
+ - формирует ответ клиенту
+ - отправляет ответ клиенту
+ - имеет параметры командной строкиЖ
+ - -p <port> - TCP-порт для работы(по дефолту 7777)
+ - -a <addr> - IP-адрес для прослушивания ( по умолчанию все доступные адреса)
+"""
+import sys
+# import json
+from socket import socket, AF_INET, SOCK_STREAM
+from jim.utils import get_message, send_message
+from jim.config import *
+
+#Func формирования ответа
+def presence_response(presence_message):
+    """
+    Формирование ответа клиенту
+    :param presence_message: Словарь presence запроса
+    :return: Словарь ответа
+    """
+    #Делаем проверки
+    if ACTION in presence_message and presence_message[ACTION] == PRESENCE and TIME in presence_message and isinstance(presence_message[TIME], float):
+        #Если все хорошо
+        return {RESPONSE: 200}
+    else:
+        #Шлем код ошибки
+        return {RESPONSE: 400, ERROR: 'Неверный запрос'}
+
+#Запускаем сервер
+if __name__ == '__main__':
+    #Создается TCP-сокет сервера
+    server = socket(AF_INET, SOCK_STREAM)
+    #Получаем аргументы скрипта
+    # ip - адрес
+    #если ip указан в параметрах
+    try:
+        addr = sys.argv[1]
+    #если ip не указан в параметрах
+    except IndexError:
+        addr = ''
+    # порт
+    #если порт указан в параметрах
+    try:
+        port = int(sys.argv[2])
+    #если порт не указан в параметрах
+    except IndexError:
+        port = 7777
+    #Если порт не целое число
+    except ValueError:
+        print('Порт должен быть целым числом')
+        # sys.exit(0)
+
+    server.bind((addr, port)) #присваивает порт 7777
+    server.listen(5)
+    while 1:
+        #Принять запрос на соединение
+        client, addr = server.accept()
+        #принимает сообщение клиента
+        presence = get_message(client)
+        print(presence)
+        print(client)
+        #формирует ответ
+        response = presence_response(presence)
+        #отправляет ответ клиенту
+        send_message(client, response)
+        client.shutdown()
+        client.close()
